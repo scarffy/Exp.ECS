b7b56bde6ab33ae358bba18aeba210df69dc9839
Hakimin <34879344+scarffy@users.noreply.github.com>
2023-10-31 17:19:12 +0800

    spike: movement aspect

--- Assets/Scripts/Aspects/MoveToPositionAspect.cs
+++ Assets/Scripts/Aspects/MoveToPositionAspect.cs
@@ -1,13 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 namespace ECSProgramming
 {
+    /// <summary>
+    /// For some reason the aspect move doesn't work
+    /// </summary>
     public readonly partial struct MoveToPositionAspect : IAspect
     {
+        //! One of the main purpose of Aspect is to group together logics
+        private readonly Entity entity;
+        
+        //! Can be regular component or other aspect
+        //! We want to write new value to local transform
+        private readonly RefRW<LocalTransform> transformAspect;
         private readonly RefRO<Speed> speed;
         private readonly RefRW<TargetPosition> targetPosition;
+
+        public void Move(float deltaTimem, Random random)
+        {
+            float3 direction = math.normalize(targetPosition.ValueRW.targetPosition - transformAspect.ValueRO.Position);
+            transformAspect.ValueRW.Position += direction * speed.ValueRO.speedValue * deltaTime;
+
+            float reachedTargetPosition = .5f;
+            if (math.distance(transformAspect.ValueRO.Position, targetPosition.ValueRW.targetPosition) <
+                reachedTargetPosition)
+            {
+                targetPosition.ValueRW.targetPosition = GetRandomPosition(random);
+            }
+        }
+
+        private float3 GetRandomPosition(Random random)
+        {
+            return new float3(
+                random.NextFloat(0f,15f), 
+                0,
+                random.NextFloat(0f,15f)
+            );
+        }
     }
 }
--- Assets/Scripts/Systems/MovingSystemBase.cs
+++ Assets/Scripts/Systems/MovingSystemBase.cs
@@ -4,6 +4,7 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 namespace ECSProgramming
 {
@@ -14,17 +15,42 @@ namespace ECSProgramming
     {
         protected override void OnUpdate()
         {
+            //! This does work
             // Entities.ForEach ((ref LocalTransform aspect, in Speed speed, in TargetPosition targetPos) =>
             // {
-                Entities.ForEach ((ref LocalTransform aspect, in Speed speed) =>
-                {
-                // float3 direction = math.normalize(targetPos.targetPosition - aspect.Position);
-                // aspect.Position += new float3(SystemAPI.Time.DeltaTime * speed.speedValue * direction);
-                aspect.Position += new float3(SystemAPI.Time.DeltaTime * speed.speedValue,0,0);
-            }).Run();
+                // Entities.ForEach ((ref LocalTransform aspect, in Speed speed) =>
+                // {
+                 // float3 direction = math.normalize(targetPos.targetPosition - aspect.Position);
+                 // aspect.Position += new float3(SystemAPI.Time.DeltaTime * speed.speedValue * direction);
+                // aspect.Position += new float3(SystemAPI.Time.DeltaTime * speed.speedValue,0,0);
+            // }).Run();
                 
             //! If we use Schedule. It will run on main thread
             //! If we use ScheduleParallel, it will run on multiple thread
+
+          
+            //! This also does work but it will not work with ISystemBase
+            foreach (var(speed, transform, target) in SystemAPI.Query<RefRO<Speed>,RefRW<LocalTransform>, RefRW<TargetPosition>>())
+            {
+                transform.ValueRW = transform.ValueRO.Translate(GetRandomPosition() * speed.ValueRO.speedValue * SystemAPI.Time.DeltaTime);
+                
+                float reachedTargetDistance = .5f;
+                if (math.distance(transform.ValueRO.Position, target.ValueRW.targetPosition) < reachedTargetDistance)
+                {
+                    target.ValueRW.targetPosition = GetRandomPosition();
+                }
+            }
+        }
+
+        private float3 GetRandomPosition()
+        {
+           Random random = new Random(1);
+
+            return new float3(
+                random.NextFloat(0f, 15f),
+                0,
+                random.NextFloat(0f,15f)
+            );
         }
     }
 }
